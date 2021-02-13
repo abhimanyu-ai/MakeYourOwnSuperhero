7e85799cad551a69153438af52672278b2de9ead
Abhimanyu Krishna! <74185786+abhimanyu-ai@users.noreply.github.com>
2021-02-13 10:49:26 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -27,3 +27,58 @@ function new_image(get_image) {
         canvas.add(block_object);
     });
 }
+window.addEventListener("keydown", my_keydown);
+function my_keydown(e) {
+    keypressed=e.keyCode;
+    console.log(keypressed);
+    if (e.shiftKey == true && keypressed == '80') {
+        console.log("Shift and P pressed together");
+        block_width=block_width+10;
+        block_height=block_height+10;
+        document.getElementById('current_width').innerHTML=block_width;
+        document.getElementById('current_height').innerHTML=block_height;
+    }
+    if (e.shiftKey == true && keypressed == '77') {
+        console.log("Shift and M pressed together");
+        block_width=block_width-10;
+        block_height=block_height-10;
+        document.getElementById('current_width').innerHTML=block_width;
+        document.getElementById('current_height').innerHTML=block_height;
+    }
+    if (keypressed == '70') {
+        new_image('ironman_face.png');
+        console.log("F");
+    }
+    if (keypressed == '66') {
+        new_image('spiderman_body.png');
+        console.log("B");
+    }
+    if (keypressed == '76') {
+        new_image('hulk_legs.png');
+        console.log("L");
+    }
+    if (keypressed == '82') {
+        new_image('thor_right_hand.png');
+        console.log("R");
+    }
+    if (keypressed == '72') {
+        new_image('captain_america_left_hand.png');
+        console.log("H");
+    }
+    if (keypressed == '38') {
+        up();
+        console.log("Up");
+    }
+    if (keypressed == '40') {
+        down();
+        console.log("Down");
+    }
+    if (keypressed == '37') {
+        left();
+        console.log("Left");
+    }
+    if (keypressed == '39') {
+        right();
+        console.log("Right");
+    }
+}
